7b7078e3853bdbdb62063cd216ed03e35af32f6b
Lukas Günther <hallo@lguenther.com>
2024-08-13 09:08:48 +0200

    Update runUpdate.sh

--- tools/runUpdate.sh
+++ tools/runUpdate.sh
@@ -4,7 +4,7 @@ if [ "$1x" = "-dx" -o "$1x" = "--debugx" ]; then
         shift
 		DEBUG=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8001
 fi
-java -cp target/wikidata-query-tools-*-SNAPSHOT-jar-with-dependencies.jar $DEBUG \
+java -cp target/own.jar $DEBUG \
     -DRDFRepositoryMaxPostSize=20000000 \
     org.wikidata.query.rdf.tool.Update --sparqlUrl http://localhost:9999/bigdata/namespace/wdq/sparql $*
 
